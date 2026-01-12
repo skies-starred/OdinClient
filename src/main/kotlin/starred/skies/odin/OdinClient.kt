62831b980ce36b139182f2e38dfcab369b77f2c4
skies-starred <skies.starred@gmail.com>
2026-01-12 10:04:09 +0530

    Bump to 0.1.2-r1

--- src/main/kotlin/starred/skies/odin/OdinClient.kt
+++ src/main/kotlin/starred/skies/odin/OdinClient.kt
@@ -30,7 +30,7 @@ object OdinClient : ClientModInitializer {
     private var lastInstall: String by mainFile.string("lastInstall")
     private var send: Boolean = true
 
-    const val MOD_VERSION: String = /*$ mod_version*/ "0.1.2"
+    const val MOD_VERSION: String = /*$ mod_version*/ "0.1.2-r1"
 
     override fun onInitializeClient() {
         ModuleManager.registerModules(moduleConfig, *modulesToRegister)
