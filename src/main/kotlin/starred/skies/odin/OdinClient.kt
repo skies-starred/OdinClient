c1e1ff7998b631c9e6429d394972283e5b67c2d8
skies-starred <skies.starred@gmail.com>
2026-03-13 07:22:49 +0000

    bump(version): 0.1.6-r2

--- src/main/kotlin/starred/skies/odin/OdinClient.kt
+++ src/main/kotlin/starred/skies/odin/OdinClient.kt
@@ -39,7 +39,7 @@ object OdinClient : ClientModInitializer {
     private var lastInstall: String by mainFile.string("lastInstall")
     private var send: Boolean = true
 
-    const val MOD_VERSION: String = /*$ mod_version*/ "0.1.6-r1"
+    const val MOD_VERSION: String = /*$ mod_version*/ "0.1.6-r2"
     val moduleConfig: ModuleConfig = ModuleConfig("odinClient")
     val joinListeners = mutableListOf<() -> Unit>()
 
