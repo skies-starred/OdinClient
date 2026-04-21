67b803daa51538a4d3adf013e3355a77ebd52cf0
camber <150540051+camberX@users.noreply.github.com>
2026-04-21 11:41:25 +0200

    feat(Highlight): wither esp and tracers (#18)
    Co-authored-by: skies-starred <skies.starred@gmail.com>

--- src/main/kotlin/starred/skies/odin/events/EntityMetadataEvent.kt
+++ src/main/kotlin/starred/skies/odin/events/EntityMetadataEvent.kt
@@ -4,4 +4,4 @@ import com.odtheking.odin.events.core.CancellableEvent
 import net.minecraft.network.protocol.Packet
 import net.minecraft.world.entity.Entity
 
-class EntityMetadataEvent(val entity: Entity, packet: Packet<*>) : CancellableEvent()
+class EntityMetadataEvent(val entity: Entity, val packet: Packet<*>) : CancellableEvent()
--- src/main/kotlin/starred/skies/odin/features/impl/cheats/Highlight.kt
+++ src/main/kotlin/starred/skies/odin/features/impl/cheats/Highlight.kt
@@ -6,7 +6,6 @@ import com.odtheking.odin.clickgui.settings.impl.ColorSetting
 import com.odtheking.odin.clickgui.settings.impl.MapSetting
 import com.odtheking.odin.clickgui.settings.impl.SelectorSetting
 import com.odtheking.odin.events.RenderEvent
-import com.odtheking.odin.events.TickEvent
 import com.odtheking.odin.events.WorldEvent
 import com.odtheking.odin.events.core.on
 import com.odtheking.odin.features.Module
@@ -15,13 +14,17 @@ import com.odtheking.odin.utils.Colors
 import com.odtheking.odin.utils.noControlCodes
 import com.odtheking.odin.utils.render.drawStyledBox
 import com.odtheking.odin.utils.renderBoundingBox
+import com.odtheking.odin.utils.renderPos
 import com.odtheking.odin.utils.skyblock.dungeon.DungeonUtils
+import com.odtheking.odin.utils.skyblock.dungeon.M7Phases
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.boss.wither.WitherBoss
 import net.minecraft.world.entity.decoration.ArmorStand
 import net.minecraft.world.entity.monster.EnderMan
 import net.minecraft.world.entity.player.Player
+import starred.skies.odin.events.EntityMetadataEvent
 import starred.skies.odin.utils.Skit
+import starred.skies.odin.utils.drawTracer
 
 object Highlight : Module(
     name = "Highlight (C)",
@@ -30,72 +33,103 @@ object Highlight : Module(
 ) {
     private val depthCheck by BooleanSetting("Depth Check", false, desc = "Disable to enable ESP")
     private val highlightStar by BooleanSetting("Highlight Starred Mobs", true, desc = "Highlights starred dungeon mobs.")
+    private val starredTracer by BooleanSetting("Starred mobs tracers", desc = "Draws a tracer to the starred mobs.")
     val color by ColorSetting("Highlight color", Colors.WHITE, true, desc = "The color of the highlight.")
     private val renderStyle by SelectorSetting("Render Style", "Outline", listOf("Filled", "Outline", "Filled Outline"), desc = "Style of the box.")
     private val hideNonNames by BooleanSetting("Hide non-starred names", true, desc = "Hides names of entities that are not starred.")
-
     private val teammateClassGlow by BooleanSetting("Teammate Class Glow", true, desc = "Highlights dungeon teammates based on their class color.")
+    private val highlightWither by BooleanSetting("Highlight Withers", true, desc = "Highlights Necron, Goldor, Storm and Maxor.")
+    private val witherColor by ColorSetting("Wither ESP Color", Color(255, 0, 0, 1f), true, desc = "The color of the wither highlight.")
+    private val witherTracer by BooleanSetting("Wither Tracer", true, desc = "Draws a tracer to the wither boss in P3 section 4.")
+    private val customTracer by BooleanSetting("Custom tracer", desc = "Draws a tracer to the mobs added manually")
 
-    private val dungeonMobSpawns = hashSetOf("Lurker", "Dreadlord", "Souleater", "Zombie", "Skeleton", "Skeletor", "Sniper", "Super Archer", "Spider", "Fels", "Withermancer", "Lost Adventurer", "Angry Archaeologist", "Frozen Adventurer")
-    // https://regex101.com/r/QQf502/1
+    private val dungeonMobSpawns = hashSetOf("Lurker", "Dreadlord", "Souleater", "Zombie", "Skeleton", "Skeletor", "Sniper", "Super Archer", "Spider", "Fels", "Withermancer", "Lost Adventurer", "Angry Archaeologist", "Frozen Adventurer", "Shadow Assassin")
     private val starredRegex = Regex("^.*✯ .*\\d{1,3}(?:,\\d{3})*(?:\\.\\d+)?[kM]?❤$")
 
     val highlightMap by MapSetting("highlightMap", mutableMapOf<String, Color>())
 
-    private val customEntities = hashMapOf<Entity, Color>()
-    private val starredEntities = hashSetOf<Entity>()
+    private val starredIds = hashSetOf<Int>()
+    private val customIds = hashMapOf<Int, Color>()
+    private val witherIds = hashSetOf<Int>()
+    private val checkedIds = hashSetOf<Int>()
 
     init {
         OdinMod.logger.debug("Loaded ${highlightMap.entries.size}")
 
-        on<TickEvent.End> {
-            val world = mc.level ?: return@on
+        on<EntityMetadataEvent> {
+            if (!DungeonUtils.inDungeons) return@on
+            if (!entity.isAlive) return@on
 
-            val bool = highlightStar && DungeonUtils.inDungeons && !DungeonUtils.inBoss
-            val bool0 = highlightMap.isNotEmpty()
-
-            starredEntities.clear()
-            customEntities.clear()
-
-            if (!bool && !bool0) return@on
-
-            for (stand in world.entitiesForRendering()) {
-                if (stand !is ArmorStand || !stand.isAlive) continue
-
-                val rawName = stand.displayName?.string?.noControlCodes?.takeIf { !it.equals("armor stand", true)} ?: continue
-                val nameLower = rawName.lowercase()
-
-                if (bool && dungeonMobSpawns.any(rawName::contains)) {
-                    val starred = starredRegex.matches(rawName)
-                    if (hideNonNames && stand.isInvisible && !starred) continue
-                    if (starred) stand.fn()?.let(starredEntities::add)
+            when {
+                highlightWither && entity is WitherBoss && entity.isPowered -> {
+                    witherIds.add(entity.id)
                 }
 
-                if (bool0) {
-                    val match = highlightMap.entries.firstOrNull { nameLower.contains(it.key) } ?: continue
-                    stand.fn(true)?.let { customEntities[it] = match.value }
+                highlightStar && !DungeonUtils.inBoss && entity is Player && entity != mc.player && entity.gameProfile.name.contains("Shadow Assassin") -> {
+                    starredIds.add(entity.id)
+                }
+
+                (highlightStar || highlightMap.isNotEmpty()) && !DungeonUtils.inBoss && entity is ArmorStand -> {
+                    val rawName = entity.displayName?.string?.noControlCodes?.takeIf { !it.equals("armor stand", true) } ?: return@on
+                    val nameLower = rawName.lowercase()
+
+                    if (highlightStar && dungeonMobSpawns.any(rawName::contains)) {
+                        val starred = starredRegex.matches(rawName)
+                        if (hideNonNames && entity.isInvisible && !starred) return@on
+                        if (starred && checkedIds.add(entity.id)) {
+                            entity.fn(true)?.let { starredIds.add(it.id) }
+                        }
+                    }
+
+                    if (highlightMap.isNotEmpty()) {
+                        val match = highlightMap.entries.firstOrNull { nameLower.contains(it.key) } ?: return@on
+                        entity.fn(true)?.let { customIds[it.id] = match.value }
+                    }
                 }
             }
         }
 
         on<RenderEvent.Extract> {
-            if (customEntities.isEmpty() && starredEntities.isEmpty()) return@on
+            if (customIds.isEmpty() && starredIds.isEmpty() && witherIds.isEmpty()) return@on
 
-            starredEntities.removeIf { !it.isAlive }
-            customEntities.entries.removeIf { !it.key.isAlive }
+            val world = mc.level ?: return@on
+            val bool0 = starredTracer
+            val bool1 = witherTracer && DungeonUtils.getF7Phase() == M7Phases.P3
+            val bool2 = customTracer
 
-            starredEntities.forEach {
-                drawStyledBox(it.renderBoundingBox, color, renderStyle, depthCheck)
+            starredIds.forEach { id ->
+                val entity = world.getEntity(id) ?: return@forEach
+                drawStyledBox(entity.renderBoundingBox, color, renderStyle, depthCheck)
+
+                if (bool0) {
+                    drawTracer(entity.renderPos, color, depth = depthCheck)
+                }
             }
 
-            customEntities.forEach { (entity, color) ->
+            witherIds.forEach { id ->
+                val entity = world.getEntity(id) ?: return@forEach
+                drawStyledBox(entity.renderBoundingBox, witherColor, renderStyle, depthCheck)
+
+                if (bool1) {
+                    drawTracer(entity.renderPos, witherColor, depth = depthCheck)
+                }
+            }
+
+            customIds.forEach { (id, color) ->
+                val entity = world.getEntity(id) ?: return@forEach
                 drawStyledBox(entity.renderBoundingBox, color, renderStyle, depthCheck)
+
+                if (bool2) {
+                    drawTracer(entity.renderPos, color, depth = depthCheck)
+                }
             }
         }
 
         on<WorldEvent.Load> {
-            starredEntities.clear()
-            customEntities.clear()
+            starredIds.clear()
+            customIds.clear()
+            witherIds.clear()
+            checkedIds.clear()
         }
     }
 
@@ -112,8 +146,8 @@ object Highlight : Module(
     private fun isValidEntity(entity: Entity, vis: Boolean = false): Boolean =
         when (entity) {
             is ArmorStand -> false
-            is WitherBoss -> false
             is Player -> entity.uuid.version() == 2 && entity != mc.player
+            is WitherBoss -> true
             else -> entity is EnderMan || (vis || !entity.isInvisible)
         }
 
