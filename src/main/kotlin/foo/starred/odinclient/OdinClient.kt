4557912adc51f29940282f6fa5732da6ef0fecae
mizly <64050405+mizly@users.noreply.github.com>
2026-09-02 13:57:44 -0400

    bump(version): 0.3.2-r1

--- src/main/kotlin/foo/starred/odinclient/OdinClient.kt
+++ src/main/kotlin/foo/starred/odinclient/OdinClient.kt
@@ -40,7 +40,7 @@ object OdinClient : ClientModInitializer {
     private var lastInstall: String by mainFile.string("lastInstall")
     private var send: Boolean = true
 
-    const val MOD_VERSION: String = /*$ mod_version*/ "0.3.1-r1"
+    const val MOD_VERSION: String = /*$ mod_version*/ "0.3.2-r1"
     val moduleConfig: ModuleConfig = ModuleConfig("odinClient")
     val joinListeners = mutableListOf<() -> Unit>()
 
